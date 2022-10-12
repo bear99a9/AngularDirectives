95dbacc1049fc7310dc6cad9f714da5faeb2913d
Sean Edwards <sean.edwards@teachingpersonnel.com>
2022-10-12 10:33:08 +0100

    Made directive interactive

--- src/app/shared-directives/better-highlight.directive.ts
+++ src/app/shared-directives/better-highlight.directive.ts
@@ -1,13 +1,22 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit{
 
+  @HostListener('mouseenter') mouseover(eventData: Event){
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event){
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
+
+  }
+
   constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit(){
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
   }
 }
